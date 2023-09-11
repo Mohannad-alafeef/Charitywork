c5d98bf9d08764df9cb816c8f72ee9865f1df969
tasneem mohamad <tmoh4218@gmail.com>
2023-09-11 20:38:42 +0300

    AboutUs Done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUSComponent } from './about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'about',
+    component:AboutUSComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
