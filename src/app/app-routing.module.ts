f3e3c333bcb19c233f7d536b775482d7aaefaaf5
mohannad <mohannadafif1998@gmail.com>
2023-09-18 17:32:15 +0300

    home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AboutUSComponent } from './home/about-us/about-us.component';
-import { ContactUsComponent } from './home/contact-us/contact-us.component';
-import { HomePageComponent } from './home/home-page/home-page.component';
 
 const routes: Routes = [
   {
