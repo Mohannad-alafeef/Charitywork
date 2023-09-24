0e0301c642b5d9f39d5fed4739a1bf73247c3ae9
mohannad <mohannadafif1998@gmail.com>
2023-09-24 15:03:58 +0300

    reply ready

--- src/app/admin/contact/contact.component.ts
+++ src/app/admin/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { faQuoteLeft,faUser,faExclamation,faReply,faCheck, IconDefinition} from '@fortawesome/free-solid-svg-icons'
@@ -15,13 +16,21 @@ export class ContactComponent {
   unread=Const.Unread;
   replied=Const.Replied;
   @ViewChild('deleteDialog') deleteDialog!: TemplateRef<any>;
+  @ViewChild('replyDialog') replyDialog!: TemplateRef<any>;
  // issuesReport:any=[{}];
   ngOnInit(): void {
     refresh2();
     
   }
-  constructor(public contactService:ContactService,private router:Router,private dialog:MatDialog){
-
+  replyForm:FormGroup;
+  constructor(public contactService:ContactService,private router:Router,private dialog:MatDialog,private fb:FormBuilder){
+    this.replyForm = fb.group(
+      {
+        reciverEmail:['',Validators.required],
+        subject:['',Validators.required],
+        body:['',Validators.required]
+      }
+    )
   }
   getIcon(status:number):IconDefinition{
     if(status == Const.Unread){
@@ -49,17 +58,40 @@ export class ContactComponent {
     return this.router.url;
   }
   selectedId:number = -1 ;
-  openDeleteDialog(id:number){
+  currentIndex = -1;
+  openDeleteDialog(id:number,index:number){
     this.selectedId = id;
+    this.currentIndex = index;
     console.log(id);
     
     this.dialog.open(this.deleteDialog);
   }
   deleteMessage(){
     console.log('deleted '+this.selectedId);
-    this.contactService.deleteMessage(this.selectedId);
+    this.contactService.deleteMessage(this.selectedId,this.currentIndex);
   }
-  updateStatus(id:number,status:number){
-    this.contactService.updateStatus(id,status);
+  updateStatus(id:number,status:number,index:number){
+    this.contactService.updateStatus(id,status,this.currentIndex);
+  }
+  name:string='';
+  openReplyDialog(name:string,email:string,contactId:number,index:number){
+    this.currentIndex = index;
+    this.selectedId = contactId;
+    this.name = name;
+    this.replyForm.reset();
+    this.replyForm.controls['reciverEmail'].setValue(email);
+    this.dialog.open(this.replyDialog,{
+      maxWidth:'100%',
+      minWidth:'50%'
+    });
+  }
+  send(){
+    console.log(this.replyForm.value);
+    this.contactService.send(this.replyForm.value,this.selectedId,this.currentIndex)
+    
+  }
+  sendAndDelete(){
+    console.log(this.replyForm.value);
+    this.contactService.sendAndDelete(this.replyForm.value,this.selectedId,this.currentIndex);
   }
 }
--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,13 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { Const } from '../shared/Const';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
-  recentContact:any;
-  allContact:any=[];
+  recentContact:Array<any>=[];
+  allContact:Array<any>=[];
   constructor(private http: HttpClient,private toaster:ToastrService) {}
 
   async getContact(){
@@ -29,29 +30,23 @@ export class ContactService {
     this.recentContact = res.sort((n1:any,n2:any)=>n1.contactId<n2.contactId).slice(0,4);
    });
   }
-  deleteMessage(id:number){
+  deleteMessage(id:number,index:number){
     this.http.delete(`https://localhost:7081/api/Contact/delete/${id}`).subscribe({
       next:()=>{
         this.toaster.success('deleted','',{
           positionClass:'toast-bottom-right'
         });
-        this.allContact = this.allContact.filter((obj:any)=>obj.contactId !=id);
+        
+        this.allContact = this.allContact.splice(index,1);
         this.recentContact = this.recentContact.filter((obj:any)=>obj.contactId !=id);
       }
     })
   }
-  updateStatus(id:number,status:number){
+  updateStatus(id:number,status:number,index:number){
     this.http.put(`https://localhost:7081/api/Contact/updateStatus`,{contactId:id,contactStatus:status}).subscribe({
       next:()=>{
-        this.toaster.success('Readed','',{
-          positionClass:'toast-bottom-right'
-        });
-        this.allContact = this.allContact.map((obj:any)=>{
-          if(obj.contactId == id){
-            obj.contactStatus = status
-          }
-          return obj;
-        });
+ 
+        this.allContact.at(index).contactStatus = status;
         this.recentContact = this.recentContact.map((obj:any)=>{
           if(obj.contactId == id){
             obj.contactStatus = status
@@ -61,4 +56,34 @@ export class ContactService {
       }
     })
   }
+  send(body:any,id:number,index:number):Promise<any>{
+    let sendPromise = new Promise<any>((res,rej)=>{
+      this.http.post('https://localhost:7081/api/Email/sendNormal',body).subscribe({
+      next:(r)=>{
+        this.toaster.success('Message has Sent','',{
+          positionClass:'toast-bottom-right'
+        });
+        setTimeout(()=>{
+
+          res(r);
+        },1000)
+      },
+      error:(err)=>{
+        this.toaster.error(err,'',{
+          positionClass:'toast-bottom-right'
+        });
+        rej(err);
+      }
+    })
+    });
+    return sendPromise.then(()=>{
+      this.updateStatus(id,Const.Replied,index);
+    })
+   
+  }
+  async sendAndDelete(body:any,id:number,index:number){
+    await this.send(body,id,index);
+    this.deleteMessage(id,index);
+  }
+
 }
