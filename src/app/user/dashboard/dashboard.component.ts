1970737c82a5de2c131ff7aafa5889f8f388a5c4
walaa200 <2018980126@ses.yu.edu.jo>
2023-09-26 09:15:32 +0300

    user Testimonial

--- src/app/user/dashboard/dashboard.component.ts
+++ src/app/user/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+declare function refresh2(): any;
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
-
+  ngOnInit(): void {
+    refresh2();
+  }
 }
